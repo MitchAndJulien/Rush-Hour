af53f8824f66296a978abcc12446ff3bddff78ee
julienzhu0 <julienzhu0@gmail.com>
2017-03-28 10:42:22 -0400

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -6,7 +6,38 @@ public class Driver {
 		Board play=new Board();
 		//create a new board
 		
-		//create 10 levels
+		
+		ObejctiveCar red=new ObjectiveCar(int x, int y);
+		
+		Horzblockercar2 car1= new Horzblockercar2(int x, int y);
+		Horzblockercar2 car2= new Horzblockercar2(int x, int y);
+		Horzblockercar3 car3= new Horzblockercar3(int x, int y);
+		Horzblockercar3 car4= new Horzblockercar3(int x, int y);
+		
+		
+		Vertblockercar2 car5= new Vertblockercar2(int x, int y);
+		Vertblockercar2 car6= new Vertblockercar2(int x, int y);
+		Vertblockercar3 car7= new Vertblockercar3(int x, int y);
+		Vertblockercar3 car8= new Vertblockercar3(int x, int y);
+		//replace each x,y with numbers
+		
+		
+		
+
+
+		
+		
+		
+		//Board is like
+		//[0xy 1x 2x 3x]
+		//[1y]
+		//[2y]
+		//[3y]
+		
+		
+		
+		
+		//create 10 levels so ten different drivers for each level
 		while(objectivecomplete==false){
 			//continue running the code
 		}
