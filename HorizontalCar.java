206591387119d8bb736b92dcd86306a1f240eb8a
julienzhu0 <julienzhu0@gmail.com>
2017-03-28 10:00:47 -0400

    Update HorizontalCar.java

--- HorizontalCar.java
+++ HorizontalCar.java
@@ -1,5 +1,11 @@
 package testme;
 
 public class HorizontalCar {
-
+  private int length;
+  public HorizontalCar(int x1, int x2){
+    if(x1>0 && x2<board.getlength){
+      //continue
+    }else{
+      
+    }
 }
