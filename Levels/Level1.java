95a221d0e69b94f30fb2d32a69357160ab863d22
mitch1887 <mitch1887@gmail.com>
2017-03-28 10:53:25 -0400

    Update Level1.java

--- Levels/Level1.java
+++ Levels/Level1.java
@@ -2,7 +2,30 @@
 public class Level1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// horizontal
+		player = (2,2);
+		
+		// horizontal
+		2blockcar1 = (0,0);
+		
+		// horizontal
+		2blockcar2 = (4,4);
+		
+		// vertical
+		2blockcar3 = (0,4);
+		
+		// vertical
+		3blockcar1 = (0,1);
+		
+		// vertical
+		3blockcar2 = (5,0);
+		
+		// vertical
+		3blockcar3 = (3,1);
+		
+		// horizontal
+		3blockcar3 = (2,5);
 
 	}
 
