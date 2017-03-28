2ddd751bf64890e3870dc65af1c9b5f4a58c1b7c
julienzhu0 <julienzhu0@gmail.com>
2017-03-28 09:50:30 -0400

    Update Board.java

--- Board.java
+++ Board.java
@@ -4,6 +4,9 @@ public class Board {
 
   public Board(){
     car[][] traffic=new car[6][6];
+    //or some type of similar array for storage of data
     
+    
+    //after initialization that should be it for now
   
 }
