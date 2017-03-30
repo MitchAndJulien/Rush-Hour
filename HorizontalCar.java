028f82ddd6d4b3d113563b43d4fae417079371cd
andersondav <andersondavid2000@gmail.com>
2017-03-30 10:57:50 -0400

    Update HorizontalCar.java

--- HorizontalCar.java
+++ HorizontalCar.java
@@ -2,7 +2,9 @@ package testme;
 
 public class HorizontalCar {
   private int length;
+  private String type;
   public HorizontalCar(int x1, int x2){
+    type = "Horizontal Car";
     if(x1>0 && x2<board.getlength){
       //continue
     }else{
