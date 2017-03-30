b85e34abaac9a31b5d8e2e8d664795a79a2d6d6a
andersondav <andersondavid2000@gmail.com>
2017-03-30 10:52:45 -0400

    Update VerticalCar.java

--- VerticalCar.java
+++ VerticalCar.java
@@ -2,4 +2,16 @@ package testme;
 
 public class VerticalCar {
 
+  
+  public void moveCar(int x, int y){
+		if(validmove==true){
+		posx = x;
+		posy = y;
+		} else {
+			System.out.println("Invalid move");
+			//make an error sound
+		}
+	}
+  
+  
 }
