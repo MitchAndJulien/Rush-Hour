311cd92503a385240882eb14fdf583effe537ffc
andersondav <andersondavid2000@gmail.com>
2017-03-30 10:59:12 -0400

    Update ObjectiveCar.java

--- ObjectiveCar.java
+++ ObjectiveCar.java
@@ -1,10 +1,14 @@
 package testme;
 
 public class ObjectiveCar extends Car {
+	private int posx;
+	private int posy;
+	private String type;
 
 	public ObjectiveCar(int x1, int x2){
 		super(x1,x2);
 		//put this with horizontal cars
+		type = "Objective Car";
 	}
 	
 	public checkifcomplete(int){
