b676c1b0a02c055b6a12d4e02cee093c72e076cc
andersondav <andersondavid2000@gmail.com>
2017-03-30 10:32:54 -0400

    Update ObjectiveCar.java

--- ObjectiveCar.java
+++ ObjectiveCar.java
@@ -10,5 +10,14 @@ public class ObjectiveCar extends Car {
 	public checkifcomplete(int){
 		
 	}
+	public void moveCar(int x, int y){
+		if(validmove==true){
+		posx = x;
+		posy = y;
+		} else {
+			System.out.println("Invalid move");
+			//make an error sound
+		}
+	}
 	
 }
