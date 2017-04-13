91fb0a6e6ed3785e7fd4b3eee457c9f749c03beb
mitch1887 <mitch1887@gmail.com>
2017-04-13 10:24:30 -0400

    Update Level6.java

--- Levels/Level6.java
+++ Levels/Level6.java
@@ -1,8 +1,40 @@
 
-public class Level1 {
+public class Level6 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// h
+		player = (2,2);
+		
+		// h
+		2blockcar1 = (0,0);
+		
+		// h
+		2blockcar2 = (0,1);
+		
+		// h
+		2blockcar3 = (0,3);
+		
+		// v
+		2blockcar4 = (2,3);
+		
+		// v
+		2blockcar5 = (3,0);
+		
+		// v
+		2blockcar6 = (0,4);
+		
+		// v
+		3blockcar = (3,2);
+		
+		// v
+		3blockcar = (4,1);
+		
+		// v
+		3blockcar = (5,1);
+		
+		// h
+		3blockcar = (3,5);
 
 	}
 
