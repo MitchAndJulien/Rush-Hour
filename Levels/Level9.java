99509a12fd04a210d7134ceebcde63c58f520b14
mitch1887 <mitch1887@gmail.com>
2017-04-19 10:23:26 -0400

    Update Level9.java

--- Levels/Level9.java
+++ Levels/Level9.java
@@ -1,8 +1,41 @@
 
-public class Level1 {
+public class Level9 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		// h
+		player = (1,2);
+		
+		// h
+		2blockcar1 = (2,0);
+		
+		// h
+		2blockcar2 = (4,0);
+		
+		// h
+		2blockcar3 = (4,1);
+		
+		// v
+		2blockcar4 = (2,4);
+		
+		// v
+		2blockcar5 = (3,1);
+		
+		// v
+		2blockcar6 = (5,1);
+		
+		// v
+		2blockcar7 = (5,4);
+		
+		// h
+		3blockcar1 = (1,3);
+		
+		// v
+		3blockcar2 = (0,3);
+		
+		// v
+		3blockcar3 = (4,2);
 
 	}
 
