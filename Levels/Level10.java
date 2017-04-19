01fd38833588a9d30fa37058f2d5d0f362f135f8
mitch1887 <mitch1887@gmail.com>
2017-04-19 10:27:28 -0400

    Update Level10.java

--- Levels/Level10.java
+++ Levels/Level10.java
@@ -1,8 +1,43 @@
 
-public class Level1 {
+public class Level10 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// h
+		player = (2,2);
+		
+		// h
+		2blockcar1 = (0,0);
+		
+		// h
+		2blockcar2 = (0,1);
+		
+		// v
+		2blockcar3 = (2,0);
+		
+		// h
+		2blockcar4 = (4,0);
+		
+		// h
+		2blockcar5 = (0,5);
+		
+		// v
+		2blockcar6 = (3,4);
+		
+		// h
+		2blockcar7 = (4,4);
+		
+		// h
+		2blockcar8 = (4,5);
+		
+		// v
+		3blockcar1 = (0,2);
+		
+		// v
+		3blockcar2 = (5,1);
+		
+		// h
+		3blockcar3 = (1,3);
 
 	}
 
