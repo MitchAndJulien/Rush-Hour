49101becf24854b2b53722b79882b35b74fb625e
mitch1887 <mitch1887@gmail.com>
2017-04-25 09:53:30 -0400

    Update Level7.java

--- Levels/Level7.java
+++ Levels/Level7.java
@@ -3,33 +3,24 @@ public class Level7 {
 
 	public static void main(String[] args) {
 		
-		// h
-		player = (2,2);
+		player = (2,2, "horizontal");
 		
-		// h
-		2blockcar1 = (3,0);
 		
-		// h
-		2blockcar2 = (3,3);
+		2blockcar1 = (3,0, "horizontal");
 		
-		// v
-		2blockcar3 = (1,0);
+		2blockcar2 = (3,3, "horizontal");
 		
-		// v
-		2blockcar4 = (4,0);
+		2blockcar3 = (1,0, "vertical");
 		
-		// v
-		2blockcar5 = (5,0);
+		2blockcar4 = (4,0, "vertical");
 		
-		// v
-		2blockcar6 = (3,1);
+		2blockcar5 = (5,0, "vertical");
 		
-		// v
-		2blockcar7 = (5,2);
+		2blockcar6 = (3,1, "vertical");
 		
-		// v
-		2blockcar8 = (3,4);
-
+		2blockcar7 = (5,2, "vertical");
+		
+		2blockcar8 = (3,4, "vertical");
+		
 	}
-
 }
