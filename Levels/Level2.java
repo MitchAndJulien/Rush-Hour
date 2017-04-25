d7cd6c7e1713ff673c5703e31799b63f0c89ad92
mitch1887 <mitch1887@gmail.com>
2017-04-25 09:46:50 -0400

    Update Level2.java

--- Levels/Level2.java
+++ Levels/Level2.java
@@ -3,39 +3,29 @@ public class Level2 {
 
 	public static void main(String[] args) {
 		
-		// horizontal
-		player = (1,2);
-		
-		// vertical
-		2blockcar1 = (0,0);
-		
-		// vertical
-		2blockcar2 = (3,1);
-		
-		// vertical
-		2blockcar3 = (4,2);
-		
-		// horizontal
-		2blockcar4 = (1,5);
-		
-		// vertical
-		2blockcar5 = (2,4);
-		
-		// horizontal
-		2blockcar6 = (4,5);
-		
-		// horizontal
-		2blockcar7 = (5,4);
+		player = (1,2 "horizontal");
 		
 		
-		// horizontal
-		3blockcar1 = (5,0);
+		2blockcar1 = (0,0, "vertical");
 		
-		// horizontal
-		3blockcar1 = (2,3);
+		2blockcar2 = (3,1, "vertical");
 		
-		// vertical
-		3blockcar1 = (5,1);
+		2blockcar3 = (4,2, "vertical");
+		
+		2blockcar4 = (1,5, "horizontal");
+		
+		2blockcar5 = (2,4, "vertical");
+		
+		2blockcar6 = (4,5 "horizontal");
+		
+		2blockcar7 = (5,4 "horizontal");
+		
+		
+		3blockcar1 = (5,0 "horizontal");
+		
+		3blockcar2 = (2,3 "horizontal");
+		
+		3blockcar3 = (5,1, "vertical");
 		
 
 	}
