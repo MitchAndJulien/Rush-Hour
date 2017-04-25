73d5db8b027d67f061b3313584ffde6a6a120af6
mitch1887 <mitch1887@gmail.com>
2017-04-25 09:54:50 -0400

    Update Level8.java

--- Levels/Level8.java
+++ Levels/Level8.java
@@ -3,48 +3,35 @@ public class Level8 {
 
 	public static void main(String[] args) {
 		
-		// h
-		player = (1,2);
+		player = (1,2, "horizontal");
 		
-		// h
-		2blockcar1 = (1,3);
 		
-		// h
-		2blockcar2 = (1,4);
+		2blockcar1 = (1,3, "horizontal");
 		
-		// h
-		2blockcar3 = (1,5);
+		2blockcar2 = (1,4, "horizontal");
 		
-		// h
-		2blockcar4 = (3,1);
+		2blockcar3 = (1,5, "horizontal");
 		
-		// h
-		2blockcar5 = (4,0);
+		2blockcar4 = (3,1, "horizontal");
 		
-		// h
-		2blockcar6 = (5,3);
+		2blockcar5 = (4,0, "horizontal");
 		
-		// v
-		2blockcar7 = (2,2);
+		2blockcar6 = (5,3, "horizontal");
 		
-		// v
-		2blockcar8 = (3,2);
+		2blockcar7 = (2,2, "vertical");
 		
-		// v
-		2blockcar9 = (4,1);
+		2blockcar8 = (3,2, "vertical");
 		
-		// v
-		2blockcar10 = (2,4);
+		2blockcar9 = (4,1, "vertical");
 		
-		// v
-		3blockcar = (5,0);
+		2blockcar10 = (2,4, "vertical");
 		
-		// h
-		3blockcar = (5,4);
 		
-		// h
-		3blockcar = (5,5);
-
+		3blockcar1 = (5,0, "vertical");
+		
+		3blockcar2 = (5,4, "horizontal");
+		
+		3blockcar3 = (5,5, "horizontal");
+		
 	}
-
 }
