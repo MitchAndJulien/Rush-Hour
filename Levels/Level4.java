ef5e69b8d800065fe4a615a87c2039ed1ce702b2
mitch1887 <mitch1887@gmail.com>
2017-05-01 10:11:37 -0400

    Update Level4.java

--- Levels/Level4.java
+++ Levels/Level4.java
@@ -3,21 +3,21 @@ public class Level4 {
 
 	public static void main(String[] args) {
 		
-		ObjectiveCar player = new ObjectiveCar(2, 1, 0, trafficJam, srt);
+		ObjectiveCar player = new ObjectiveCar(2, 1, 1, trafficJam, srt);
 		
 		
-		TwoVertCar car1 = new TwoVertCar(3, 2, 0, trafficJam, srt);
+		TwoVertCar car1 = new TwoVertCar(3, 2, 2, trafficJam, srt);
 		
-		TwoVertCar car2 = new TwoVertCar(4, 5, 0, trafficJam, srt);
+		TwoVertCar car2 = new TwoVertCar(4, 5, 3, trafficJam, srt);
 		
 		
-		ThreeVertCar car3 = new ThreeVertCar(0, 0, 0, trafficJam, srt);
+		ThreeVertCar car3 = new ThreeVertCar(0, 0, 4, trafficJam, srt);
 		
-		ThreeVertCar car4 = new ThreeVertCar(0, 3, 0, trafficJam, srt);
+		ThreeVertCar car4 = new ThreeVertCar(0, 3, 5, trafficJam, srt);
 		
-		ThreeHorzCar car5 = new ThreeHorzCar(3, 3, 0, trafficJam, srt);
+		ThreeHorzCar car5 = new ThreeHorzCar(3, 3, 6, trafficJam, srt);
 		
-		ThreeHorzCar car6 = new ThreeHorzCar(5, 2, 0, trafficJam, srt);
+		ThreeHorzCar car6 = new ThreeHorzCar(5, 2, 7, trafficJam, srt);
 		
 	}
 }
