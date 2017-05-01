e20b0d304120c35c9a04b2bb38d46c68ee741a9f
mitch1887 <mitch1887@gmail.com>
2017-05-01 10:13:08 -0400

    Update Level8.java

--- Levels/Level8.java
+++ Levels/Level8.java
@@ -3,35 +3,35 @@ public class Level8 {
 
 	public static void main(String[] args) {
 		
-		ObjectiveCar player = new ObjectiveCar(2, 0, 0, trafficJam, srt);
+		ObjectiveCar player = new ObjectiveCar(2, 0, 1, trafficJam, srt);
 		
 		
-		TwoHorzCar car1 = new TwoHorzCar(3, 0, 0, trafficJam, srt);
+		TwoHorzCar car1 = new TwoHorzCar(3, 0, 2, trafficJam, srt);
 		
-		TwoHorzCar car2 = new TwoHorzCar(4, 0, 0, trafficJam, srt);
+		TwoHorzCar car2 = new TwoHorzCar(4, 0, 3, trafficJam, srt);
 		
-		TwoHorzCar car3 = new TwoHorzCar(5, 0, 0, trafficJam, srt);
+		TwoHorzCar car3 = new TwoHorzCar(5, 0, 4, trafficJam, srt);
 		
-		TwoHorzCar car4 = new TwoHorzCar(1, 2, 0, trafficJam, srt);
+		TwoHorzCar car4 = new TwoHorzCar(1, 2, 5, trafficJam, srt);
 		
-		TwoHorzCar car5 = new TwoHorzCar(0, 3, 0, trafficJam, srt);
+		TwoHorzCar car5 = new TwoHorzCar(0, 3, 6, trafficJam, srt);
 		
-		TwoHorzCar car6 = new TwoHorzCar(3, 4, 0, trafficJam, srt);
+		TwoHorzCar car6 = new TwoHorzCar(3, 4, 7, trafficJam, srt);
 		
-		TwoVertCar car7 = new TwoVertCar(2, 2, 0, trafficJam, srt);
+		TwoVertCar car7 = new TwoVertCar(2, 2, 8, trafficJam, srt);
 		
-		TwoVertCar car8 = new TwoVertCar(2, 3, 0, trafficJam, srt);
+		TwoVertCar car8 = new TwoVertCar(2, 3, 9, trafficJam, srt);
 		
-		TwoVertCar car9 = new TwoVertCar(1, 4, 0, trafficJam, srt);
+		TwoVertCar car9 = new TwoVertCar(1, 4, 10, trafficJam, srt);
 		
-		TwoVertCar car10 = new TwoVertCar(4, 2, 0, trafficJam, srt);
+		TwoVertCar car10 = new TwoVertCar(4, 2, 2, trafficJam, srt);
 		
 		
-		ThreeVertCar car11 = new ThreeVertCar(0, 5, 0, trafficJam, srt);
+		ThreeVertCar car11 = new ThreeVertCar(0, 5, 3, trafficJam, srt);
 		
-		ThreeHorzCar car12 = new ThreeHorzCar(4, 3, 0, trafficJam, srt);
+		ThreeHorzCar car12 = new ThreeHorzCar(4, 3, 4, trafficJam, srt);
 		
-		ThreeHorzCar car13 = new ThreeHorzCar(5, 3, 0, trafficJam, srt);
+		ThreeHorzCar car13 = new ThreeHorzCar(5, 3, 5, trafficJam, srt);
 		
 	}
 }
