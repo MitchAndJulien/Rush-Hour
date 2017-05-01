43b4f44667b204ba3cf523265b36096be06a7504
Julien Zhu <julienzhu0@gmail.com>
2017-05-01 10:43:39 -0400

    Update Driver.java

--- FinalProduct/Driver.java
+++ FinalProduct/Driver.java
@@ -18,48 +18,10 @@ public class Driver{
 		//TwoHorzCar car2=new TwoHorzCar(4,4,2,trafficJam,srt);
 		ThreeHorzCar car3=new ThreeHorzCar(2,2,3,trafficJam,srt);
 		TwoVertCar car4 =new TwoVertCar(3,2,3,trafficJam,srt);
-		//Yay finally got all buttons onto one jFrame
-		
-		//this is the way to go it appears...
-		
 
+		
 		srt.createBackground(srt);
 		
-		
-		
-		
-		/**
-		 * this does not work...
-		 */
 		car0.finished(trafficJam,car0);
-		
-		
-		
-		
-		
-		
-		/**
-		 * this is some testing to see some stuff
-		 * 1. to see if I can manually add in "buttons" for background (complete)
-		 * 2. to see finishing congratulations.
-		 */
-		
-//		while(z=true){
-//			for(int x=0;x<=5;x++){
-//				if(trafficJam[x][5]==10){
-//					System.out.println("yay");
-//					JFrame level=new JFrame();
-//					level.setSize(1300,1000);
-//					level.setLayout(null);
-//					z=false;
-//					break;
-//				}
-//			}
-//		}
-
-	//the way the gui will work is that it gets the array of ints, then
-	//for ones it colors it red, blue for twos...
-	//we will assume that the developers(us) know about car conflicts...	
-		
 	}
 }
