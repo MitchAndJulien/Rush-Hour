a30ddca56200915ce15d236abd80c3708139fd42
mitch1887 <mitch1887@gmail.com>
2017-05-01 10:12:23 -0400

    Update Level6.java

--- Levels/Level6.java
+++ Levels/Level6.java
@@ -3,29 +3,29 @@ public class Level6 {
 
 	public static void main(String[] args) {
 		
-		ObjectiveCar player = new ObjectiveCar(2, 1, 0, trafficJam, srt);
+		ObjectiveCar player = new ObjectiveCar(2, 1, 1, trafficJam, srt);
 		
 		
-		TwoHorzCar car1 = new TwoHorzCar(0, 0, 0, trafficJam, srt);
+		TwoHorzCar car1 = new TwoHorzCar(0, 0, 2, trafficJam, srt);
 		
-		TwoHorzCar car2 = new TwoHorzCar(1, 0, 0, trafficJam, srt);
+		TwoHorzCar car2 = new TwoHorzCar(1, 0, 3, trafficJam, srt);
 		
-		TwoHorzCar car3 = new TwoHorzCar(3, 0, 0, trafficJam, srt);
+		TwoHorzCar car3 = new TwoHorzCar(3, 0, 4, trafficJam, srt);
 		
-		TwoVertCar car4 = new TwoVertCar(3, 2, 0, trafficJam, srt);
+		TwoVertCar car4 = new TwoVertCar(3, 2, 5, trafficJam, srt);
 		
-		TwoVertCar car5 = new TwoVertCar(0, 3, 0, trafficJam, srt);
+		TwoVertCar car5 = new TwoVertCar(0, 3, 6, trafficJam, srt);
 		
-		TwoVertCar car6 = new TwoVertCar(4, 0, 0, trafficJam, srt);
+		TwoVertCar car6 = new TwoVertCar(4, 0, 7, trafficJam, srt);
 		
 		
-		ThreeVertCar car7 = new ThreeVertCar(2, 3, 0, trafficJam, srt);
+		ThreeVertCar car7 = new ThreeVertCar(2, 3, 8, trafficJam, srt);
 		
-		ThreeVertCar car8 = new ThreeVertCar(1, 4, 0, trafficJam, srt);
+		ThreeVertCar car8 = new ThreeVertCar(1, 4, 9, trafficJam, srt);
 		
-		ThreeVertCar car9 = new ThreeVertCar(1, 5, 0, trafficJam, srt);
+		ThreeVertCar car9 = new ThreeVertCar(1, 5, 10, trafficJam, srt);
 		
-		ThreeHorzCar car10 = new ThreeHorzCar(5, 3, 0, trafficJam, srt);
+		ThreeHorzCar car10 = new ThreeHorzCar(5, 3, 2, trafficJam, srt);
 		
 	}
 }
