1affb34af1a6c3b412c293efc74ca1bfcc1536f5
Julien Zhu <julienzhu0@gmail.com>
2017-05-17 10:26:36 -0400

    Update HorzCar.java

--- Ultimate Legit Actual Final Game/HorzCar.java
+++ Ultimate Legit Actual Final Game/HorzCar.java
@@ -8,21 +8,54 @@ import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
-//this potentially should become a class and then the two and three should
-//become interfaces??? not really
 
 public class HorzCar{
 
+	/**
+	 * The row of the left side of the Horizontal car
+	 */
 	protected int row;
+	
+	/**
+	 * The column of the left side of the horizontal car
+	 */
 	protected int column;
+	
+	/**
+	 * The code for the car that determines the color
+	 */
 	protected int carCode;
+	
+	/**
+	 * Left button of the horizontal car
+	 */
 	protected JButton leftButton;
+	
+	/**
+	 * right button of the horizontal car
+	 */
 	protected JButton rightButton;
+	
+	/**
+	 * The row for the left button
+	 */
 	protected int leftButtonRow;
+	
+	/**
+	 * The column for the left button
+	 */
 	protected int leftButtonColumn;
+	
+	/**
+	 * The size of each button and each space for movement
+	 */
 	protected final int BUTTON_SIZE=150;
-	protected int lengthCode;
-				
+			
+	/**
+	 * Changes the color of the inputted button
+	 * @param b The button that one wants to change
+	 * @param code The specific color desired
+	 */
 	public void createColor(JButton b, int code){
 		if(code==1){
 			b.setBackground(Color.WHITE);
@@ -47,6 +80,15 @@ public class HorzCar{
 		}
 	}
 	
+	/**
+	 * Returns a new Button
+	 * @param leftButtonColumn The column of the left button
+	 * @param leftButtonRow The row of the left button
+	 * @param BUTTON_SIZE Size of each button
+	 * @param str The text in each button
+	 * @param code The color code of the car
+	 * @return
+	 */
 	public JButton createButton(int leftButtonColumn, int leftButtonRow, int BUTTON_SIZE, String str, int code){
 		JButton b=new JButton();
 		b.setBounds(leftButtonColumn, leftButtonRow, BUTTON_SIZE, BUTTON_SIZE);
@@ -57,19 +99,42 @@ public class HorzCar{
 		return b;
 	}
 	
-	//getters
+	/**
+	 * Getter for the row of the 2D logic array
+	 * @return The row number
+	 */
 	public int getRow(){
 		return row;
 	}
+	
+	/**
+	 * Getter for the column of the 2D logic array
+	 * @return The column number
+	 */
 	public int getColumn(){
 		return column;
 	}
+	
+	/**
+	 * Getter for the Car Color Code of 
+	 * @return The car code
+	 */
 	public int getCarCode(){
 		return carCode;
 	}
+	
+	/**
+	 * Getter for the left button row
+	 * @return The left button row
+	 */
 	public int getLeftButtonRow(){
 		return leftButtonRow;
 	}
+	
+	/**
+	 * Getter for the left button column
+	 * @return The left button column
+	 */
 	public int getLeftButtonColumn(){
 		return leftButtonColumn;
 	}
