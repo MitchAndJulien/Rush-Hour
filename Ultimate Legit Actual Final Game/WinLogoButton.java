dc245b8bd453a4f38b14ac4d244df9265158726a
andersondav <andersondavid2000@gmail.com>
2017-05-17 10:23:02 -0400

    Update WinLogoButton.java

--- Ultimate Legit Actual Final Game/WinLogoButton.java
+++ Ultimate Legit Actual Final Game/WinLogoButton.java
@@ -6,10 +6,13 @@ import javax.swing.JButton;
 
 public class WinLogoButton extends JButton{
 	
+	/**
+	 * A constructor that sets up a win button with an image
+	 */
 	public WinLogoButton(){
 		super();
 		try {
-		    Image img = ImageIO.read(getClass().getResource("RushHourMeme2.jpg")); 
+		    Image img = ImageIO.read(getClass().getResource("meme.png")); 
 		    this.setIcon(new ImageIcon(img));
 		  } catch (Exception ex) {
 		    System.out.println(ex);
