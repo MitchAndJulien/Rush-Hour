c3b56d84a1231ab980229a028ea6912f1e34ffc5
andersondav <andersondavid2000@gmail.com>
2017-05-17 10:18:21 -0400

    Update WinScreen.java

--- Ultimate Legit Actual Final Game/WinScreen.java
+++ Ultimate Legit Actual Final Game/WinScreen.java
@@ -5,10 +5,20 @@ import javax.swing.JFrame;
 
 public class WinScreen {
 
+	/**
+	 * A JFrame representing the screen displayed when a player wins
+	 */
 	private JFrame win;
 	
+	/**
+	 * A JButton that indicates the player has won with a picture/meme
+	 * and returns them to the start screen
+	 */
 	private WinLogoButton meme;
 	
+	/**
+	 * Constructor that sets up the WinScreen with a JFrame and JButton
+	 */
 	public WinScreen(){
 		win = new JFrame();
 		win.setSize(1300, 1000);
@@ -20,10 +30,18 @@ public class WinScreen {
 		win.repaint();
 	}
 	
+	/**
+	 * Sets the win screen to be visible or not
+	 * @param a true means screen is visible, false means screen is not visible
+	 */
 	public void setVisible(boolean a){
 		this.win.setVisible(a);
 	}
 	
+	/**
+	 * A getter for the win button
+	 * @return The WinButton
+	 */
 	public JButton winbutton(){
 		return this.meme;
 	}
